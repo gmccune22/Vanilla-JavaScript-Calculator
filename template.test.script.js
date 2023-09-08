a9de02b261f86a01afd884bcaed66d24b8aab994
gmccune22 <gmccune@ufl.edu>
2023-09-08 08:06:02 -0400

    Added the test template

---
+++ template.test.script.js
@@ -0,0 +1,17 @@
+/*
+  Describe blocks organize your unit tests into distinct categories of functionality.
+ */
+  describe('Calculator Unit Tests', () => {
+    test('computes addition correctly', () => {
+      console.log("works");
+      }),
+    test('computes multiplication correctly', () => {
+       console.log('works');       
+    }),
+    test('computes subtraction correctly', () => {
+      console.log('works');
+    }),
+    test('computes division correctly', () => {
+        console.log('works');
+    });
+  });
